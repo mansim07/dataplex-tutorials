ae1b5d9ad934a3589693063be475232df5a575fd
Mansi Maharana <maharanam@google.com>
2023-05-24 11:41:05 -0700

    latest changes

--- generate_data.sh
+++ generate_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
-projectId=${1} 
-
-
+cd dataplex-tutorials/data_generator
+for dt in {0..4}
+do
+    export DATE_PARTITION=$( date -d"+${dt} days" +%Y-%m-%d )
+    bash generate_sample_data.sh
+done
